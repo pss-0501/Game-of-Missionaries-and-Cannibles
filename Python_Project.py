4f6e6285593a3a7ab66afc84453e30aab43f5444
psawant5070@gmail.com <psawant5070@gmail.com>
2023-09-26 20:26:40 -0400

    Rules added

---
+++ Python_Project.py
@@ -0,0 +1,4 @@
+#Python program to illustrate Missionaries & cannibals Problem - MINI Project
+print("\n")
+print("\tGame Start\nNow the task is to move all of them to right side of the river")
+print("rules:\n1. The boat can carry at most two people\n2. If cannibals num greater than missionaries then the cannibals would eat the missionaries\n3. The boat cannot cross the river by itself with no people on board")
